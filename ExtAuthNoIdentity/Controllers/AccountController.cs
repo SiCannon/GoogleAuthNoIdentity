607ead8d20ad6176db6786235133a1c80dd2a044
Simon Cannon <simon.cannon@gmail.com>
2017-09-05 22:44:07 +0100

    Add project files.

---
+++ ExtAuthNoIdentity/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExtAuthNoIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        public IActionResult ExternalLogin(string returnUrl = null)
+        {
+            var redirectUrl = Url.Action(nameof(ExternalLoginCallback), "Account", new { ReturnUrl = returnUrl });
+            var properties = new AuthenticationProperties() { RedirectUri = redirectUrl };
+            return Challenge(properties, "Google");
+        }
+
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            return new RedirectResult(returnUrl);
+        }
+    }
+}
